d451df1896e507acaef3e989ead64a5b6ba9352a
Balogina <67536698+bumpkinfanboi@users.noreply.github.com>
2026-03-25 20:26:59 -0700

    added perks
    working on character creation and stuff

--- database.lua
+++ database.lua
@@ -9,12 +9,13 @@ local player = { -- base stats, is changed during gameplay but not permenantly (
             left_leg = 65,
             right_leg = 65,
         },
+        mental_health = 100, -- each segment of 20 has its own quote (e.g. 100 = fine, 0 = insane rambling)
         sustinance = {
             hunger = 100, -- out of 100
             hydration = 100, -- out of 100
-            addictions = { -- addictions.cigarette == true looks alot nicer compared to addictions.is_cigarette == true
-                cigarette = false,
-                cigarette_need = 0,
+            addictions = { -- addictions.nicotine == true looks alot nicer compared to addictions.is_nicotine == true
+                nicotine = false,
+                nicotine_need = 0,
                 alcohol = false,
                 alcohol_need = 0,
             }
@@ -45,7 +46,8 @@ local maps = { -- TODO: use this somewhere to shut up luachecker
     types_of_trench = {"T", "B", "D", "H", "C"} -- Trench (basic), Bunker [entrance], Dugout, Howitzer, Canteen
 }
 local arg1, arg2, arg3 = ... -- shut up i know some are unused luachecker (btw all files are run thru luachecker)
-local introduction = [[
+local introduction = {
+step1 = [[
 Select a class.
 Rifleman:
     Basic infantry. Comes equipped with a rifle and a gas mask.
@@ -54,9 +56,32 @@ Raider:
 Medic:
     Medical specialist. Issued with a handgun and basic medical equipment.
 Officer:
-    High ranking commander. Has a whistle for rallying troops and a handgun.]]
+    High ranking commander. Has a whistle for rallying troops and a handgun.]],
+step2 = [[
+Select two or more perks.
+Scav:
+    Always the opportunist, you pilfer whatever you can, even if it's not the best.
+    Containers have 10% more items but at 0-30% reduced durability.
+Insomniac:
+    The artillery sounds are getting to you. You find it hard to stay asleep.
+    You wake up one hour early but you have -1 move range and a random 10% negative modifier on accuracy)
+Underage:
+    Eager for the supposed spoils and glory of war, you did whatever you could to join the military. You lied on your recruitment papers.
+    You have a permenant mental health boost and you require 15% less rations. Your carry capacity is reduced by 20%, you are +30% succeptable to addictions, and you take +10% damage.
+blitzkrieg - when take dmg move speed up
+mudinvincible - immune to mud debuffs but reduced total move speed
+Cigarette Addict:
+    War is hell. Healthy coping mechanisms are in short supply. You've grown reliant on Nicotine.
+    When craving Nicotine, you have a 20% accuracy debuff and hunger drains 30% faster. When not craving Nicotine, gain +1 perception range. Gain a permenant Nicotine addiction.
+Alcohol Addict:
+    You hate it here. Everything gives you nightmares. Alcohol stops the bad thoughts.
+    When not on Alcohol, your mental health drains 5% per hour. Alcohol's positive effect on your mood is doubled. Gain a permenant Alcohol addiction.
+
+]] -- Underage gives a permenant +19% mood buff. The exact numbers are never shown to the player. 0-20% is worst, 21-40% is second worst.
+}
+local class_specific_dialogue = {}
 if arg1 == "introduction" then
-    return introduction
+    return introduction[tostring("step"..arg2)]
 end
 if arg1 == "player" then
     return player
--- main.lua
+++ main.lua
@@ -1,56 +1,59 @@
+require("database")
 local database = assert(loadfile("database.lua"))
 local to_save = nil
 local file = nil
 local input = nil
 local function save_write(save_location, data)
-    file = io.open("savefile", "r")
-    to_save = file:read("a")
+    file = io.open("savefile", "a+")
+    to_save = file:read("*all")
     file:close()
     if save_location == "class" then
-        if string.find(to_save, "class") ~= "fail" then
+        if string.find(to_save, "class") then
             to_save = string.gsub(to_save, "{class = %a+}", "{class = "..data.."}")
-        else to_save = "{class = "..data.."}"
+        else to_save = to_save.."\n{class = "..data.."}"
         end
     end
-    file = io.open("savefile", "w+")
-    file:write(to_save)
-    file:close()
 end
 
 local function save_game()
-    -- TODO: write all data to savefile by copying database table(s)
+    if to_save == nil then print("Nothing to save!") return end
+    file = io.open("savefile", "w")
+    file:write(to_save)
+    file:close()
+    print("Saved!")
 end
 
 local function create_character()
-    -- TODO: take all the stuff from gameloop() if input == start and shove it here
+    print(database("introduction", 1))
+    print('Type "back" to return.')
+    while true do
+        input = io.read()
+        if input == "back" then print("Returning to main") break end -- quits back to game_loop() 
+        if input == "quit" then
+            save_game()
+            os.exit()
+        end
+        if input == "Rifleman" then -- start of step 1
+            save_write("class", "Rifleman")
+        elseif input == "Raider" then
+            save_write("class", "Raider")
+        elseif input == "Medic" then
+            save_write("class", "Medic")
+        elseif input == "Officer" then
+            save_write("class", "Officer")
+        else print("Not a class.") end
+        print(database("introduction", 2))-- start of step 2
+    end
 end
 
 local function game_loop()
     while true do
         input = io.read()
-        if input == "start" then
-            create_character()
-            print(database("introduction"))
-            input = io.read()
-            if input == "Rifleman" then
-                save_write("class", "Rifleman")
-            end
-            if input == "Raider" then
-                save_write("class", "Raider")
-            end
-            if input == "Medic" then
-                save_write("class", "Medic")
-            end
-            if input == "Officer" then
-                save_write("class", "Officer")
-            end
+        if input == "start" then create_character()
         elseif input == "quit" then
-            print("Do you want to save?")
-            input = io.read()
-            if input == "y" or input == "Y" or input == "yes" or input "Yes" then save_game() break
-            elseif input == "n" or input == "no" or input == "N" or input == "No" then break
-            else print("Not a yes or no answer! (type either y, Y, yes, Yes, or n, N, no, No)")
-            end
+            print("Saving!")
+            save_game()
+            break
         end
     end
 end
